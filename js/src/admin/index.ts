d32875c8461eaaa4f42c07f96d8e4b26023d07a3
dashka <dashka.gb@gmail.com>
2026-04-05 01:24:19 +0800

    Open links in new tab

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -3,5 +3,5 @@ import app from 'flarum/admin/app';
 export { default as extend } from './extend';
 
 app.initializers.add('dashzeveg-open-links-in-new-tab', () => {
-  console.log('[dashzeveg/flarum-open-links-in-new-tab] Hello, admin!');
+  // console.log('[dashzeveg/flarum-open-links-in-new-tab] Hello, admin!');
 });
--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,7 +1,28 @@
 import app from 'flarum/forum/app';
-
-export { default as extend } from './extend';
+import { extend } from 'flarum/common/extend';
+import CommentPost from 'flarum/forum/components/CommentPost';
 
 app.initializers.add('dashzeveg-open-links-in-new-tab', () => {
-  console.log('[dashzeveg/flarum-open-links-in-new-tab] Hello, forum!');
+  // console.log('[dashzeveg/flarum-open-links-in-new-tab] Hello, forum!');
+
+  const processLinks = (element: Element) => {
+    (element.querySelectorAll('.Post-body a') as NodeListOf<HTMLAnchorElement>).forEach((link) => {
+      link.setAttribute('target', '_blank');
+
+      const href = link.getAttribute('href') || '';
+      if (href.startsWith('/d/')) {
+        link.setAttribute('rel', 'noopener noreferrer');
+      } else {
+        link.setAttribute('rel', 'noopener noreferrer nofollow');
+      }
+    });
+  };
+
+  extend(CommentPost.prototype, 'oncreate', function () {
+    processLinks(this.element);
+  });
+
+  extend(CommentPost.prototype, 'onupdate', function () {
+    processLinks(this.element);
+  });
 });
