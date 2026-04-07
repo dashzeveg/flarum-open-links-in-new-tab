2a60f2a1bb17068202bd5488dc4a7ce525fac9f3
dashka <dashka.gb@gmail.com>
2026-04-07 20:14:55 +0800

    #2

--- extend.php
+++ extend.php
@@ -8,9 +8,29 @@ use Laminas\Diactoros\Uri;
 return [
     (new Extend\Link)
         ->setTarget(function (Uri $uri, Uri $siteUrl, array $attributes): string {
-            return $uri->getHost() === $siteUrl->getHost() ? '_self' : '_blank';
+            // return $uri->getHost() === $siteUrl->getHost() ? '_self' : '_blank';
+            return '_blank';
         })
         ->setRel(function (Uri $uri, Uri $siteUrl, array $attributes): string {
-            return $uri->getHost() === $siteUrl->getHost() ? 'ugc' : 'noopener ugc nofollow';
+            // return $uri->getHost() === $siteUrl->getHost() ? 'ugc' : 'noopener ugc nofollow';
+            $internalPrefixes = [
+                '/d/',
+                '/t/',
+                '/u/',
+                '/following',
+                '/tags',
+                '/settings',
+                '/blog',
+            ];
+
+            $path = $uri->getPath();
+
+            foreach ($internalPrefixes as $prefix) {
+                if (str_starts_with($path, $prefix)) {
+                    return 'noopener';
+                }
+            }
+
+            return 'noopener noreferrer ugc nofollow';
         }),
 ];
